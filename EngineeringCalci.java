c93848cb0a1ead7e2d0dc022626245b9cc653e6a
Abhang Paturkar <abhangpaturkar0709@gmail.com>
2024-01-19 13:39:07 +0530

    Initial commit

---
+++ EngineeringCalci.java
@@ -0,0 +1,70 @@
+// Engineering Calculator
+/*
+	Class EngineeringCalci contains function for different mathematical calculation
+*/
+
+public class EngineeringCalci {
+	/*
+	 * Function will take argument as two Integer number then
+	 * Add this Number and return result in Integer.
+	 */
+	public static int additionTwoNumber(int num1, int num2) {
+		int result; // Initialize variable result to store result
+		result = num1 + num2; // Adding two number
+		return result; // Return result of operation perform
+	}
+
+	/*
+	 * Function will take argument as two Integer number then
+	 * Subtract num2 from num1 and retern result in Integer.
+	 */
+	public static int subtractionTwoNumber(int num1, int num2) {
+		int result; // Initialize variable result to store result
+		result = num1 - num2; // Subtract num2 from num1
+		return result; // Return result of operation performed
+	}
+
+	/*
+	 * Function will take argument as two Integer number then
+	 * Multiply these Number and return result in Integer.
+	 */
+	public static int mulitplyTwoNumber(int num1, int num2) {
+		int result; // Initialize variable result to store result
+		result = num1 * num2; // Mulitplying two number
+		return result; // Return result of operation performed
+	}
+
+	/*
+	 * Function will take argument as two Integer number then
+	 * Divide num1 by num2 and return result in Integer.
+	 */
+	public static int divideTwoNumber(int num1, int num2) {
+		int result; // Initialize variable result to store result
+		result = num1 / num2; // Divide num1 by num2
+		return result; // Return result of operation performed
+	}
+
+	/*
+	 * Function will take argument as base and index then
+	 * multiply base value itself for index value of times and return in Integer.
+	 */
+	public static int power(int base, int index) {
+		int result = 1; // Initialize varibable result = 1 because 1 Mulitply to any number will be that
+						// number.
+		for (int i = 1; i <= index; i++) { // Loop iterate for index value of times
+			result = result * base; // Mulitply result and base value for each iteration
+		}
+		return result; // Return result of operation performed.
+	}
+
+	/*
+	 * Function will take argument as value and percent then
+	 * calculate percentage for given value and return result.
+	 */
+	public static int percentage(int value, int percent) {
+		int result; // Initialize variavle result to store result
+		result = (value * percent) / 100; // Calculate percentage by dividing percent by 100 and multiply result to
+											// value
+		return result; // Return result of operation performed.
+	}
+}
